23e5bd5dfbc6112f925eaa181bfc27a07d8a2067
Flatiron School <stvik2@gmail.com>
2019-10-30 20:51:40 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return self.end_with?('.')
   end
 
   def question?
-
+    return self.end_with?('?')
   end
 
   def exclamation?
-
+    return self.end_with?('!')
   end
 
   def count_sentences
-
+    delimiters = [".", "?", "!"]
+    split_sentence = self.split(Regexp.union(delimiters))
+    #binding.pry
+    return split_sentence.map {|value| split_sentence.delete("")}.count
+    #return split_sentence.count
   end
 end
+
+"This, well, is a sentence. This is too!! And so is this, I think? Woo...".count_sentences
